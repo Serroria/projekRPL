2c60b92e96e44269f02e562e69d55584d48b6847
Serroria <merylien.jovanda14@gmail.com>
2025-05-12 15:01:28 +0700

    menambahkan button

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -455,19 +455,20 @@
         <img src="kencurberas.png" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Beras Kencur</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 8.500,00</p>
-        <button onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+      
       </a>
       <a href="#" class="group">
         <img src="buyungupik.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Buyung upik</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 7.600,00</p>
-        <button onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
       <a href="#" class="group">
         <img src="sari-temulawak.png" alt="Jamu Temulawak" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Sari Temulawak</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 85.000,00</p>
-        <button onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
       
 
@@ -475,7 +476,7 @@
         <img src="encok.png" alt="Jamu Temulawak" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Encok</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 2.000,00</p>
-        <button onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
 
       <!-- More products... -->
