5c0c560634bd74b600ee642529612c60e8d59e76
Ariella Chandra Naya <chandranaya02@gmail.com>
2025-05-12 15:10:43 +0700

    edit button

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -456,14 +456,15 @@
         <h3 class="mt-4 text-sm text-gray-700">Beras Kencur</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 8.500,00</p>
         <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
-      
       </a>
+      
       <a href="#" class="group">
         <img src="buyungupik.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Buyung upik</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 7.600,00</p>
         <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
+
       <a href="#" class="group">
         <img src="sari-temulawak.png" alt="Jamu Temulawak" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Sari Temulawak</h3>
@@ -483,28 +484,28 @@
         <img src="{{ asset('jamu-bersalin.png') }}" alt="Jamu Pegal Linu" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Bersalin</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 3.000,00</p>
-        <button href="#">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
 
-            <a href="#" class="group">
+      <a href="#" class="group">
         <img src="{{ asset('jamu-klingsir.png') }}" alt="Jamu Klingsir" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Klingsir</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 18.000,00</p>
-        <button href="#">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
 
-            <a href="#" class="group">
+      <a href="#" class="group">
         <img src="{{ asset('jamu-gemuk-sehat.png') }}" alt="Jamu Gemuk Sehat" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Gemuk Sehat</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 18.000,00</p>
-        <button href="#">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
 
-            <a href="#" class="group">
+      <a href="#" class="group">
         <img src="{{ asset('jamu-galian-montok.png') }}" alt="Jamu Galian Montok" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Galian Montok</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 18.000,00</p>
-        <button href="#">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
       </a>
 
 
