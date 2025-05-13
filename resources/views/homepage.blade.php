6f3f7f44cc13d8e923d9e520a99b23a0f11fb0c7
Serroria <merylien.jovanda14@gmail.com>
2025-05-14 00:25:40 +0700

    Menambahkan link shopee di button dan menambahkan katalog baru

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -37,31 +37,52 @@
       </a>
 
       <a href="#" class="group">
-        <img src="{{ asset('jamu-bersalin.png') }}" alt="Jamu Pegal Linu" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
+        <img src="{{ asset('jamu-bersalin.png') }}" alt="Jamu Bersalin" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Bersalin</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 3.000,00</p>
-        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/QreMDxF';">BELI</button>
       </a>
 
       <a href="#" class="group">
         <img src="{{ asset('jamu-klingsir.png') }}" alt="Jamu Klingsir" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Klingsir</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 18.000,00</p>
-        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/uYFb8gH';">BELI</button>
       </a>
 
       <a href="#" class="group">
         <img src="{{ asset('jamu-gemuk-sehat.png') }}" alt="Jamu Gemuk Sehat" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Gemuk Sehat</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 18.000,00</p>
-        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/azKBPh4';">BELI</button>
       </a>
 
       <a href="#" class="group">
         <img src="{{ asset('jamu-galian-montok.png') }}" alt="Jamu Galian Montok" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
         <h3 class="mt-4 text-sm text-gray-700">Jamu Galian Montok</h3>
         <p class="mt-1 text-lg font-medium text-gray-900">Rp. 18.000,00</p>
-        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://shopee.co.id/davidnicolas4?categoryId=100001&entryPoint=ShopByPDP&itemId=43550536931';">BELI</button>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/NGR8EEQ';">BELI</button>
+      </a>
+
+       <a href="#" class="group">
+        <img src="{{ asset('jamu-galian-delimaPutih.png') }}" alt="Jamu Galian Delima" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
+        <h3 class="mt-4 text-sm text-gray-700">Jamu Galian Delima Putih</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">Rp. 2.500,00</p>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/xm9G4A7';">BELI</button>
+      </a>
+
+       <a href="#" class="group">
+        <img src="{{ asset('jamu-galian-parem.png') }}" alt="Jamu Galian Parem" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
+        <h3 class="mt-4 text-sm text-gray-700">Jamu Galian Delima Parem</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">Rp. 4.500,00</p>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/t2ULzo8';">BELI</button>
+      </a>
+
+       <a href="#" class="group">
+        <img src="{{ asset('jamu-galian-singset.png') }}" alt="Jamu Galian Singset" class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
+        <h3 class="mt-4 text-sm text-gray-700">Jamu Galian Delima Singset</h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">Rp. 2.000,00</p>
+        <button button class="bg-orange-950 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="location.href='https://id.shp.ee/35HB7nQ';">BELI</button>
       </a>
 
 
