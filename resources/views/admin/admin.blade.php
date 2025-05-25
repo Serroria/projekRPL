45469124b36185552eb3a249f3ce1ccced76910c
Serroria <merylien.jovanda14@gmail.com>
2025-05-26 01:35:52 +0700

    admin button

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -93,7 +93,21 @@
             <td data-th="Harga">Rp{{ number_format($product->harga, 0, ',', '.') }}</td>
 
             <td data-th="Aksi" >
-                <a href="{{ route('admin', $product->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                <!-- From Uiverse.io by aaronross1 --> 
+              <!-- From Uiverse.io by mrhyddenn --> 
+              <a href="{{ route('admin', $product->id) }}">  
+              <button>
+                Edit
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path
+                    fill="currentColor"
+                    d="M20 17h2v2H2v-2h2v-7a8 8 0 1 1 16 0v7zm-2 0v-7a6 6 0 1 0-12 0v7h12zm-9 4h6v2H9v-2z"
+                    ></path>
+                </svg>
+                </button>
+            </a>
+
+                {{-- <a href="{{ route('admin', $product->id) }}" class="btn btn-sm btn-warning">Edit</a> --}}
                 <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display: inline;" onsubmit="return confirm('Yakin ingin menghapus?');">
                 @csrf
                 @method('DELETE')
