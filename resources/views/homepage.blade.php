a128b991685a06cedbb477f132b7d10e6d66df2a
Serroria <merylien.jovanda14@gmail.com>
2025-05-25 20:41:19 +0700

    .

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -68,8 +68,9 @@
             BELI
           </button>
         </div>
+        </div>
           @endforeach
-      </div>
+      
 
     <!-- <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
       <div class="max-w-sm mx-auto product-card" data-category="minumanHerbal">
