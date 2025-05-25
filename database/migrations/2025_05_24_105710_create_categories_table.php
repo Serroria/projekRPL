9ba4f277ebb7f0f77b45bf31cfa49393a23f6cf8
Ariella Chandra Naya <chandranaya02@gmail.com>
2025-05-25 19:49:27 +0700

    up

--- database/migrations/2025_05_24_105710_create_categories_table.php
+++ database/migrations/2025_05_24_105710_create_categories_table.php
@@ -4,20 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('gambar');
-            $table->text('deskripsi')->nullable();
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->decimal('harga', 10, 2);
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ class CreateProductsTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
-};
+}
