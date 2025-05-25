0832d93aa3ab6a93fcc143d0aaf690fc093393e4
Ariella Chandra Naya <chandranaya02@gmail.com>
2025-05-26 00:31:50 +0700

    banner

---
+++ resources/views/cart/show.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/cart/show.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cart Page</title>
+</head>
+<body>
+    <h1>Ini halaman keranjang belanja</h1>
+</body>
+</html>
